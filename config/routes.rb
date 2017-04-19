091bcc1331505420edc5d13a7ccfc417046e2cc3
CharlieKB <charlieforce1999@gmail.com>
2017-04-18 19:49:18 -0700

    add order history pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Etsydemo::Application.routes.draw do
-  
+
   resources :orders
   devise_for :users
   resources :listings do
+  resources :orders, only: [:new, :create]
   resources :orders
  end
 
-  get "pages/about"
+ get "pages/about"
   get "pages/contact"
   get 'seller' => "listings#seller"
+  get 'sales' => "orders#sales"
+  get 'purchases' => "orders#purchases"
 
   root 'listings#index'
 
